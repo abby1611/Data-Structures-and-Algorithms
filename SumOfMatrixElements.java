2798027dcc41bf675085b7896311f997db141997
Abhay Pratap Singh <apsr0316@gmail.com>
2021-06-09 16:31:42 +0530

    Add files via upload
    Not a Typical DSA problem, just uploaded it to continue my streak. 
    Super busy for the next 15 days. Will try my best to keep up the pace.

---
+++ SumOfMatrixElements.java
@@ -0,0 +1,47 @@
+package MyJavaProject;
+
+import java.io.*;
+import java.util.*;
+
+class SumOfMatrixElements
+{
+ public static void main(String args[]) throws IOException
+{
+     BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+     int t = Integer.parseInt(read.readLine());
+     while (t-- > 0) 
+     {
+         String[] S = read.readLine().split(" ");
+         int N = Integer.parseInt(S[0]);
+         int M = Integer.parseInt(S[1]);
+         int Grid[][] = new int[N][M];
+         for (int i = 0; i < N; i++)
+         {
+             String[] s = read.readLine().split(" ");
+             for (int j = 0; j < M; j++) 
+            	 Grid[i][j] = Integer.parseInt(s[j]);
+         }
+         Solution ob = new Solution();
+         System.out.println(ob.SumOfMatrixElements(N, M, Grid));
+     }
+ }
+}// Driver Code Ends
+
+
+//User function
+
+class Solution
+{
+ int SumOfMatrixElements(int N, int M, int[][] Grid)
+ {
+     int sum=0;
+     for(int i=0;i<N;i++)
+     {
+         for(int j=0;j<M;j++)
+         {
+            sum=sum+Grid[i][j]; 
+         }
+     }
+     return sum;
+ }
+}
